b3d34a173a0133a4e7e361e553c8d16cf4b4abf6
udaykumar012 <uday_kumar_kotla@epam.com>
2022-06-30 10:51:14 +0530

    angular_task1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CliComponentComponent } from './cli-component/cli-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CliComponentComponent
   ],
   imports: [
     BrowserModule,
